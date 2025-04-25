d21570f37e7a2a0cafe1e2f68336c878bfa2f908
benisamuel <benisamuel566@gmail.com>
2025-04-25 10:32:03 +0200

    Initial Commit

---
+++ src/main/java/com/umurava/umapis/UmapisApplication.java
@@ -0,0 +1,13 @@
+package com.umurava.umapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmapisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmapisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/umurava/umapis/UmapisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umurava.umapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmapisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
