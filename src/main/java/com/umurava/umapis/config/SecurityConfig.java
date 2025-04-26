86f9b1ecf52b4d0ab9e8d27ceda105ef446e8fb7
benisamuel <benisamuel566@gmail.com>
2025-04-26 20:42:13 +0200

    Added SecurityFilterChain

--- src/main/java/com/umurava/umapis/config/SecurityConfig.java
+++ src/main/java/com/umurava/umapis/config/SecurityConfig.java
@@ -4,11 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/umurava/v2/auth/**").permitAll()
+                        .requestMatchers("/api/umurava/v2/users/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+
     @Bean
     public AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
